99bfbabf45e3bb55886a8a047d2ac8108ba46ead
kleiinnn <me@klein-server.at>
2013-04-03 12:00:56 +0200

    added config file; bug fixed

---
+++ config/conf.php
@@ -0,0 +1,7 @@
+<?php
+define('LOCATION', '');
+define('SQL_HOST', '');
+define('SQL_USER', '');
+define('SQL_PASSWORD', '');
+define('SQL_DB', '');
+?>
--- general/mysql_connect.php
+++ general/mysql_connect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-	mysql_connect(SQL_HOST, SQL_USER, SQL_PASSWORD) or die('Can not connect to database: ' + mysql_error());
+	mysql_connect(SQL_HOST, SQL_USER, SQL_PASSWORD);
 	mysql_select_db(SQL_DB);
 }catch(Exception $e){
 	die("Can connect to database: " . mysql_error());
