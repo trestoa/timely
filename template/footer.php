25d141efceca7681bd6a406de390ae80558c8e87
kleiinnn <me@klein-server.at>
2013-04-13 11:06:26 +0200

    update footer

--- template/footer.php
+++ template/footer.php
@@ -6,8 +6,8 @@ if(isset($additional_script)){
 ?>
 <script src="<?php echo MAIN_URL . '/static/js/main.js' ?>"></script>
 <footer>
-<p>powered by: <a href="http://php.net/">PHP</a> and <a href="http://twitter.github.com">Twitter Bootstrap</a></p>
-<p>open source: <a href="https://github.com/kleiinnn">Github</a></p>
+<p>powered by: <a href="http://php.net/">PHP</a>, <a href="http://jquery.com/">jQuery</a> and <a href="http://twitter.github.com">Twitter Bootstrap</a></p>
+<p>open source: <a href="https://github.com/kleiinnn/Calender">Github</a></p>
 <p>&copy; 2013, <a href="http://mklein.co.at/">Markus Klein</a></p>
 <p>made with &hearts;</p>
 </footer>
