f6dceedfd127de543d6a7b9cb8c2e6b739d9d7d3
kleiinnn <me@klein-server.at>
2013-04-13 10:38:58 +0200

    fixed bug in table_user

--- install/user.sql
+++ install/user.sql
@@ -1,7 +1,7 @@
-﻿CREATE TABLE IF NOT EXISTS 'calender_user' (
-	'ID' int(10) NOT NULL AUTO_INCREMENT,
-	'email' varchar(100) NOT NULL,
-	'username' varchar(30) NOT NULL,
-	'password' varchar(40) NOT NULL,
-	PRIMARY KEY('ID')
+﻿CREATE TABLE IF NOT EXISTS `calender_user` (
+	`ID` int(10) NOT NULL AUTO_INCREMENT,
+	`email` varchar(100) NOT NULL,
+	`username` varchar(30) NOT NULL,
+	`password` varchar(40) NOT NULL,
+	PRIMARY KEY(`ID`)
 );
